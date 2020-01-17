10a58fb65e3348626cfcc500c849d5ea89ead428
Ali <connexion.founder@outlook.com>
2020-01-17 13:45:58 +0100

    Update

--- projects/amazing-search-core/src/public-api.ts
+++ projects/amazing-search-core/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of amazing-search-core
  */
 
-export * from './lib/amazing-search-core.service';
-export * from './lib/amazing-search-core.component';
-export * from './lib/amazing-search-core.module';
+export * from './lib/amazing-search.service';
+export * from './lib/amazing-search.component';
+export * from './lib/amazing-search.module';
